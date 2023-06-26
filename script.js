d6f239fcd508a6422387068e8aba01824807db47
Yash Gupta <yashgupta27690@gmail.com>
2023-06-26 21:19:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-	let s=str;
+	
 	
 
-let n=s.length;
-	s.toLowerCase();
+let n=str.length;
+	let s=str.toLowerCase();
 	let f = true;
 	for (let i = 0; i < n; i++) {
 		if (s.charAt(i)!==s.charAt(n-i-1)) {
