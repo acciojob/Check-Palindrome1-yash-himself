61e567f0cec4d1800a94b7f5b9ba2f05ee78d1c9
Yash Gupta <yashgupta27690@gmail.com>
2023-06-26 20:28:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+	while (str.includes(" ")) {
+		str.replace(" ","");
+	}
+let n=str.length;
+	for (let i = 0; i < n; i++) {
+		if (str.charAt(i)!=str.charAt(n-i-1)) {
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
